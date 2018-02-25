c633e380bcdf32a73645c9b950ea132a002b3d9a
CharlyCast <charly.castes@polytechnique.edu>
2018-02-25 14:54:26 +0100

    Merge branch 'master' of /home/Gripsou/Documents/cours/INF/CSRank with conflicts.

--- src/main/Bot.java
+++ src/main/Bot.java
@@ -13,6 +13,22 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.regex.Pattern;
 
 public class Bot extends Thread {
+//    static String baseUrl="http://www.polytechnique.edu/";
+//    static String regex="http.+polytechnique.*";
+
+//    static String baseUrl="http://www.lemonde.fr/";
+//    static String regex="http.+lemonde.*";
+
+//    static String baseUrl="https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal";
+//    static String regex="http.+wikipedia.*";
+
+//    static String baseUrl="https://www.youtube.com/";
+//    static String regex="http.+youtube.*";
+
+    static String baseUrl="https://docs.oracle.com/javase/8/docs/api/overview-summary.html";
+    static String regex="http.+docs.oracle.com.*";
+
+
 	Concurrent_WebGraph web;
     Document doc;
     HashSet<String> pages = new HashSet<>();
@@ -50,14 +66,14 @@ public class Bot extends Thread {
             String l;
             for (Element link : links) {
                 l = link.attr("href");
-                if (Pattern.matches("http.+polytechnique.*", l)) { 
+                if (Pattern.matches(regex, l)) {
                     pageLinks.add(l);
                 }
             }
 
             int index = ThreadLocalRandom.current().nextInt(0, pageLinks.size());
             int i = 0;
-            
+
             for (String link : pageLinks) {
                 pages.add(link);
                 if (i == index) {
@@ -65,11 +81,11 @@ public class Bot extends Thread {
                 }
                 i++;
             }
-            
+
 
         } catch (IOException e) {
             e.printStackTrace();
-            return "http://www.polytechnique.edu/";
+            return baseUrl;
         }
         return nextUrl;
     }
